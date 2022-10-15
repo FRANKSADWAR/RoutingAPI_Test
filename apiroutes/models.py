11b3f532f36f84268f465ca075eb29d530baa458
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-15 20:06:43 +0300

    routes

--- apiroutes/models.py
+++ apiroutes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
-# Create your models here.
+
+
+class Searoutes(gis_models.Model):
+    id = models.IntegerField(blank=False,primary_key=True)
+    fid = models.IntegerField(blank=True, null=True)
+    geom = gis_models.LineStringField(srid=4326,blank=True, null=True)
+    cost = models.FloatField(blank=True, null=True)
+    reverse_cost = models.FloatField(blank=True, null=True)
+    x1 = models.FloatField(blank=True, null=True)
+    y1 = models.FloatField(blank=True, null=True)
+    x2 = models.FloatField(blank=True, null=True)
+    y2 = models.FloatField(blank=True, null=True)
+    length = models.FloatField(blank=True, null=True)
+    source = models.BigIntegerField(blank=True, null=True)
+    target = models.BigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.target)
+
+    class Meta:
+        verbose_name_plural='Sea Routes'
+        db_table = 'searoutes'
--- apiroutes/views.py
+++ apiroutes/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from apiroutes.models import Searoutes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class RoutesApi(APIView):
+    """
+    Get a routes object from the database
+    """
+    def get_object(self,request,pk,format=None):
+        try:
+            return Searoutes.objects.get(pk=pk)
+        except Searoutes.DoesNotExist:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self,request,pk,format=None):   
+        try:
+            route=Searoutes.objects.get(pk=pk)
+            serializer = RoutesSerializer(route)
+            
+        except Searoutes.DoesNotExist:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
