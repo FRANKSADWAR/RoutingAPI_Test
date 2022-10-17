9c5c34f30c1a4057be6a7d0df297549b4b0990ee
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-17 03:57:19 +0300

    converters

--- apiroutes/urls.py
+++ apiroutes/urls.py
@@ -1,6 +1,8 @@
-from apiroutes.views import RoutesApi
+from apiroutes.views import RoutesApi, ApiRoutesGeos
 from django.urls import path, include
 
 urlpatterns = [
     path('routes/<int:pk>/',RoutesApi.as_view()),
+    path('params/<float:start_lat>/<float:start_lng>/<float:end_lat>/<float:end_lng>/',ApiRoutesGeos.as_view()),
+    
 ]
