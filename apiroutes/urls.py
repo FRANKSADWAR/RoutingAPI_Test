cdc4b863f84762019dab5417d948dc1694b3dea7
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-17 05:42:39 +0300

    yes

--- apiroutes/urls.py
+++ apiroutes/urls.py
@@ -2,11 +2,12 @@ from atexit import register
 from apiroutes import converters
 from apiroutes.views import RoutesApi, ApiRoutesGeos
 from django.urls import path, register_converter
+from rest_framework.urlpatterns import format_suffix_patterns
 
 register_converter(converters.FloatUrlParameterConverter,'float')
 
 urlpatterns = [
     path('routes/<int:pk>/',RoutesApi.as_view()),
     path('params/<float:start_lat>/<float:start_lng>/<float:end_lat>/<float:end_lng>/',ApiRoutesGeos.as_view()),
-    
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
--- apiroutes/views.py
+++ apiroutes/views.py
@@ -5,7 +5,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.decorators import action
 from apiroutes.serializers import RouteSerializer,RouteGeoSerializer
-from django.core import serializers
+from django.core.serializers import serialize
+from django.db import connection
 import psycopg2
 
 
@@ -17,21 +18,27 @@ conn = psycopg2.connect(database=database,user=user,password=password)
 def getNode(x_coords,y_coords):
     query = """
             SELECT id FROM searoutes_vertices_pgr ORDER BY 
-            geom <-> ST_SetSRID(ST_MakePoint(%s, %s),4326) LIMIT 1;
+            the_geom <-> ST_SetSRID(ST_MakePoint(%s, %s),4326) LIMIT 1;
             """
     cur = conn.cursor()
     cur.execute(query,(x_coords,y_coords))
     point = cur.fetchone()
     return point        
 
-def 
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns,row))
+        for row in cursor.fetchall()
+    ]
 
 
 class RoutesApi(APIView):
     """
     Get a routes object from the database
     """
-    def get(self,request,pk):   
+    def get(self,request,pk,format=None):   
         try:
             route=Searoutes.objects.get(pk=pk)
             serializer = RouteGeoSerializer(route)
@@ -48,22 +55,24 @@ class ApiRoutesGeos(APIView):
     PgRouting extension and then return the path as GeoJSON
     """
     def get(self,request,start_lat,start_lng,end_lat,end_lng):
-        try:
-            start_node = getNode(start_lng,start_lat)
-            end_node = getNode(end_lng,end_lat)
-            route_query =  'SELECT sea.id AS id,'
-            route_query += 'SUM(sea.length) AS length,'
-            route_query += 'SUM(sea.length) AS length,'
-            route_query += 'SUM(dij.cost) as COST, ST_Collect(geom) AS geom' 
-            route_query += 'FROM pgr_dijkstra("SELECT id, source, target, cost FROM searoutes",%s, %s) AS dij,'                           
-            route_query += 'searoutes AS sea WHERE dij.edge = sea.id GROUP BY sea.id;'
+        
+        start_node = getNode(start_lng,start_lat)
+        end_node = getNode(end_lng,end_lat)
+        route_query =  "SELECT sea.id AS id, SUM(sea.length) AS length, "
+        route_query += "SUM(dij.cost) as COST, ST_Collect(geom) AS geom "
+        route_query += "FROM pgr_dijkstra('SELECT id, source, target, cost FROM searoutes',%s, %s) AS dij, "                          
+        route_query += "searoutes AS sea WHERE dij.edge = sea.id GROUP BY sea.id "
+        with connection.cursor() as cursor:
+            cursor.execute(route_query,[start_node,end_node])
+            rows = dictfetchall(cursor)
+            print (rows)
                             
-            route_data = Searoutes.objects.raw(route_query,[start_node,end_node])
+        #route_data = Searoutes.objects.raw(route_query,[start_node,end_node])
 
-            serializer = serializers('geojson',route_data)
-            return Response(data=serializer, status=status.HTTP_200_OK)            
-        except:
-            return Response(status=status.HTTP_204_NO_CONTENT)                
+        route_data = serialize('geojson',rows)
+        return Response(data=route_data, status=status.HTTP_200_OK)            
+        
+        #return Response(status=status.HTTP_204_NO_CONTENT)                
     ## call the cursor and obtain the data
     ## call the serializet
     # return the value     
