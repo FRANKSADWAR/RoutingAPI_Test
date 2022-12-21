0feb91f7969179b227bbc77f3d7c5db7dbb5678c
FRANKSADWAR <billyfranks98@gmail.com>
2022-12-21 18:21:32 +0300

    queries

--- apiroutes/test_query.sql
+++ apiroutes/test_query.sql
@@ -1,8 +1,8 @@
 WITH route_dij AS (SELECT sea.id AS id, SUM(sea.length) AS length,SUM (dij.cost) AS cost, ST_Collect(sea.geom) AS geom FROM 
 pgr_astar('SELECT id,source,target,cost,x1,y1,x2,y2,reverse_cost FROM searoutes_noded_noded WHERE NOT ST_Intersects(sea.geom,suez.geom)',631460,627932) AS dij,searoutes AS sea WHERE dij.edge = sea.id GROUP BY sea.id) 
-                        SELECT route_dij.id,route_dij.cost, ST_AsGeoJSON(route_dij.geom) AS the_geom,
-                        route_dij.length,(SELECT SUM(ST_Length( (ST_Intersection(route.geom,eca.geom))::geography)/1852)
-                        FROM eca_areas AS eca, route_dij AS route WHERE ST_Intersects(route.geom,eca.geom)) AS eca_distance FROM route_dij
+SELECT route_dij.id,route_dij.cost, ST_AsGeoJSON(route_dij.geom) AS the_geom,
+route_dij.length,(SELECT SUM(ST_Length( (ST_Intersection(route.geom,eca.geom))::geography)/1852)
+FROM eca_areas AS eca, route_dij AS route WHERE ST_Intersects(route.geom,eca.geom)) AS eca_distance FROM route_dij
 
 
 
