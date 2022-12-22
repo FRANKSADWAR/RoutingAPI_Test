21e57a0f6781f0eac9c2907d8667cef5109504db
FRANKSADWAR <billyfranks98@gmail.com>
2022-12-22 16:16:56 +0300

    route selection

--- apiroutes/views.py
+++ apiroutes/views.py
@@ -67,21 +67,11 @@ class ApiRoutesGeos(APIView):
         start_node = getNode(start_lng,start_lat)
         end_node = getNode(end_lng,end_lat)
 
-
         query_url = """ WITH route_dij AS (SELECT sea.id AS id, SUM(sea.length) AS length,SUM (dij.cost) AS cost, ST_Collect(sea.geom) AS geom FROM pgr_astar('SELECT id,source,target,cost,x1,y1,x2,y2,reverse_cost
                         FROM searoutes_noded_noded',%s,%s) AS dij,searoutes AS sea WHERE dij.edge = sea.id GROUP BY sea.id) SELECT route_dij.id,route_dij.cost, ST_AsGeoJSON(route_dij.geom) AS the_geom,
                         route_dij.length,(SELECT SUM(ST_Length( (ST_Intersection(route.geom,eca.geom))::geography)/1852)
                         FROM eca_areas AS eca, route_dij AS route WHERE ST_Intersects(route.geom,eca.geom)) AS eca_distance FROM route_dij
                     """
-
-        query_ = " WITH route_dij AS (SELECT sea.id AS id, SUM(sea.length) AS length, "
-        query_ +=  "SUM (dij.cost) AS cost, ST_Collect(sea.geom) AS geom "
-        query_ +=  "FROM pgr_astar('SELECT id,source,target,cost,x1,y1,x2,y2,reverse_cost "
-        query_ += "FROM searoutes_noded_noded',%s,%s) AS dij, "
-        query_ += " searoutes AS sea WHERE dij.edge = sea.id GROUP BY sea.id) SELECT route_dij.id,"
-        query_ +=  " route_dij.cost, ST_AsGeoJSON(route_dij.geom) AS the_geom, "          
-        query_ += "route_dij.length,(SELECT SUM(ST_Length( (ST_Intersection(route.geom,eca.geom))::geography)/1852)"        
-        query_ += " FROM eca_areas AS eca, route_dij AS route WHERE ST_Intersects(route.geom,eca.geom)) AS eca_distance FROM route_dij"
 
         if (suez==False) and (panama==False) and (singapore==False):
             query_url = query_url  ## this is the case when they are are all false, meaning to ignore them in the routing
