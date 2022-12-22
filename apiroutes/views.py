c6514521148dc100052b000bc324739172595575
FRANKSADWAR <billyfranks98@gmail.com>
2022-12-22 16:10:44 +0300

    avoid_panama_only

--- apiroutes/views.py
+++ apiroutes/views.py
@@ -124,6 +124,12 @@ class ApiRoutesGeos(APIView):
             
         if(suez == False) and (panama == True) and (singapore == False):
             print('only panama true')
+            query_url = """WITH route_dij AS (SELECT sea.id AS id, SUM(sea.length) AS length, SUM(dij.cost) AS cost, ST_Collect(sea.geom) AS geom FROM pgr_astar('SELECT id,source,target,cost,x1,y1,x2,y2,
+                            reverse_cost FROM avoid_panama',%s,%s) AS dij, avoid_panama AS sea WHERE dij.edge = sea.id GROUP BY sea.id) SELECT route_dij.id,route_dij.cost, 
+                            ST_AsGeoJSON(route_dij.geom) AS the_geom,
+                            route_dij.length, (SELECT SUM(ST_Length((ST_Intersection(route.geom,eca.geom))::geography)/1852)
+                            FROM eca_areas AS eca, route_dij AS route WHERE ST_Intersects(route.geom,eca.geom)) AS eca_distance FROM route_dij
+                        """
 
         if(suez == True) and (panama==False) and (singapore==True):
             print('suez and singapore true')
